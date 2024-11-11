4cccfe390b75b43fd768378d02cee991f78806c3
Gabe Dunn <gabe@gabedunn.dev>
2024-11-11 14:02:05 -0800

    chore: update flake inputs, remove devenv

--- flake.nix
+++ flake.nix
@@ -2,22 +2,14 @@
   description = "Set of simple, performant neovim plugins";
 
   inputs = {
-    nixpkgs.url = "github:cachix/devenv-nixpkgs/rolling";
-    devenv.url = "github:cachix/devenv";
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
     fenix.url = "github:nix-community/fenix";
     fenix.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  nixConfig = {
-    extra-trusted-public-keys =
-      "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=";
-    extra-substituters = "https://devenv.cachix.org";
-  };
-
   outputs = inputs@{ flake-parts, nixpkgs, fenix, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
-      imports = [ inputs.devenv.flakeModule ];
       systems = [
         "x86_64-linux"
         "i686-linux"

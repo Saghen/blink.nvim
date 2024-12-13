757f1e9811a03baf0a19d5d32648061a50db833c
Liam Dyer <liamcdyer@gmail.com>
2024-12-13 12:53:32 -0500

    feat(delimiters): add sql

---
+++ lua/blink/delimiters/langs/sql.lua
@@ -0,0 +1,10 @@
+--- @type blink.delimiters.LanguageDefinition
+return {
+  delimiters = {
+    ['('] = ')',
+  },
+  line_comment = { '--' },
+  block_comment = { '/*', '*/' },
+  string = { "'" },
+  block_string = { '$$', '$$' },
+}

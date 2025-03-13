0975c09b8f1e311ea1ad8d58b6c11d8bd41eafef
Liam Dyer <liamcdyer@gmail.com>
2025-03-13 16:08:07 -0400

    feat: load rust via cpath

--- lua/blink/delimiters/init.lua
+++ lua/blink/delimiters/init.lua
@@ -11,7 +11,7 @@ function delimiters.setup(user_config)
   vim.api.nvim_set_decoration_provider(config.ns, {
     on_win = function(_, _, bufnr) return require('blink.delimiters.watcher').attach(bufnr) end,
     on_line = function(_, _, bufnr, line_number)
-      for _, match in ipairs(require('blink_delimiters').get_parsed_line(bufnr, line_number)) do
+      for _, match in ipairs(require('blink.delimiters.rust').get_parsed_line(bufnr, line_number)) do
         vim.api.nvim_buf_set_extmark(bufnr, config.ns, line_number, match.col, {
           end_col = match.col + 1,
           hl_group = config.highlights[match.stack_height % #config.highlights + 1],

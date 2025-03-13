5eec790406390c5651329b8298106e71c9183b5f
Liam Dyer <liamcdyer@gmail.com>
2025-03-13 16:02:19 -0400

    feat: incremental parsing

--- lua/blink/delimiters/init.lua
+++ lua/blink/delimiters/init.lua
@@ -8,21 +8,8 @@ function delimiters.setup(user_config)
   local config = require('blink.delimiters.config')
   config.merge_with(user_config)
 
-  local buffer_changed_ticks = {}
-
   vim.api.nvim_set_decoration_provider(config.ns, {
-    on_win = function(_, _, bufnr)
-      if vim.b[bufnr].changedtick == buffer_changed_ticks[bufnr] then return end
-      buffer_changed_ticks[bufnr] = vim.b[bufnr].changedtick
-
-      local start_time = vim.loop.hrtime()
-      local did_parse = require('blink_delimiters').parse_buffer(bufnr)
-      if did_parse and config.debug then
-        vim.print('parsing time: ' .. (vim.loop.hrtime() - start_time) / 1e6 .. ' ms')
-      end
-
-      return did_parse
-    end,
+    on_win = function(_, _, bufnr) return require('blink.delimiters.watcher').attach(bufnr) end,
     on_line = function(_, _, bufnr, line_number)
       for _, match in ipairs(require('blink_delimiters').get_parsed_line(bufnr, line_number)) do
         vim.api.nvim_buf_set_extmark(bufnr, config.ns, line_number, match.col, {
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 use std::collections::HashMap;
 use std::sync::Mutex;
 
-use delimiters::parser::{parse, Match};
+use delimiters::parser::{parse_lines, recalculate_stack_heights, Match};
 use lazy_static::lazy_static;
 use nvim_oxi::api::Buffer;
 use nvim_oxi::{Dictionary, Function, Object, Result};
@@ -14,21 +14,49 @@ lazy_static! {
 
 #[nvim_oxi::plugin]
 pub fn blink_delimiters() -> Result<Dictionary> {
-    let parse_buffer: Function<i32, Result<bool>> =
-        Function::from_fn(|bufnr: i32| -> Result<bool> {
-            let mut parsed_buffers = PARSED_BUFFERS.lock().unwrap();
+    let parse_buffer: Function<(i32, Option<i32>, Option<i32>, Option<i32>), Result<bool>> =
+        Function::from_fn(
+            |(bufnr, start_line, old_end_line, new_end_line): (
+                i32,
+                Option<i32>,
+                Option<i32>,
+                Option<i32>,
+            )|
+             -> Result<bool> {
+                let mut parsed_buffers = PARSED_BUFFERS.lock().unwrap();
 
-            let buffer = Buffer::from(bufnr);
+                let buffer = Buffer::from(bufnr);
+                let buffer_handle = buffer.handle();
 
-            let buffer_handle = buffer.handle();
-            let parsed_buffer = parse(buffer);
-            if let Some(parsed_buffer) = parsed_buffer {
-                parsed_buffers.insert(buffer_handle, parsed_buffer);
-                return Ok(true);
-            }
+                // Incremental parse
+                if let Some(existing_matches_by_line) = parsed_buffers.get_mut(&buffer_handle) {
+                    let start_line = start_line.unwrap_or(0) as usize;
+                    let old_end_line = old_end_line.unwrap_or(0) as usize;
+                    let new_end_line =
+                        new_end_line.unwrap_or(buffer.line_count().unwrap() as i32) as usize;
 
-            Ok(false)
-        });
+                    let old_range = start_line..old_end_line;
+                    let new_range = start_line..new_end_line;
+
+                    let matches_by_line = parse_lines(buffer, new_range);
+                    return match matches_by_line {
+                        None => Ok(false),
+                        Some(matches_by_line) => {
+                            existing_matches_by_line.splice(old_range, matches_by_line);
+                            recalculate_stack_heights(existing_matches_by_line);
+                            Ok(true)
+                        }
+                    };
+                }
+                // Full parse
+                else if let Some(matches_by_line) = parse_lines(buffer, 0..) {
+                    parsed_buffers.insert(buffer_handle, matches_by_line);
+                    return Ok(true);
+                }
+
+                Ok(false)
+            },
+        );
     let get_parsed_line: Function<(i32, i32), Result<Vec<Match>>> =
         Function::from_fn(|(bufnr, line_number): (i32, i32)| -> Result<Vec<Match>> {
             let parsed_buffers = PARSED_BUFFERS.lock().unwrap();

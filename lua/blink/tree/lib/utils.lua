d1e7c7c45d45c6b6a25427bf62db4db73b03ff3d
Liam Dyer <liamcdyer@gmail.com>
2025-10-21 11:47:54 -0400

    feat(tree): allow replacing terminal, ignore relative windows

--- lua/blink/tree/lib/utils.lua
+++ lua/blink/tree/lib/utils.lua
@@ -5,7 +5,7 @@ function Utils.pick_or_create_non_special_window()
   local wins = api.nvim_list_wins()
   table.insert(wins, 1, api.nvim_get_current_win())
 
-  local ignore_list = { 'blink-tree', 'terminal', 'Trouble', 'qf', 'edgy' }
+  local ignore_list = { 'blink-tree', 'Trouble', 'qf', 'edgy' }
   local ignore = {}
   for _, ft in ipairs(ignore_list) do
     ignore[ft] = true
@@ -13,9 +13,10 @@ function Utils.pick_or_create_non_special_window()
 
   -- pick the first non-special window
   for _, win in ipairs(wins) do
+    local is_relative = api.nvim_win_get_config(win).relative ~= ''
     local buf = api.nvim_win_get_buf(win)
     local options = vim.bo[buf]
-    if not ignore[options.filetype] and not ignore[options.buftype] then return win end
+    if not is_relative and not ignore[options.filetype] and not ignore[options.buftype] then return win end
   end
 
   -- create a new window if all are special
